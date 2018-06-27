f1659da48bf342d7ed787e7881a2dcadbf3694f8
olaph wagoner <zxkuqyb@gmail.com>
2018-06-27 10:41:41 -0400

    update galleryController
     * use ibm-cos-sdk
     * do not use V2 of listObjects
     * add header

--- src/controllers/galleryController.js
+++ src/controllers/galleryController.js
@@ -1,11 +1,25 @@
+// Copyright [2018] IBM Corp. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 var galleryController = function(title) {
 
-    var aws = require('aws-sdk');
+    var aws = require('ibm-cos-sdk');
     var multer = require('multer');
     var multerS3 = require('multer-s3');
     var ep = new aws.Endpoint('https://s3-api.us-geo.objectstorage.softlayer.net');
     var s3 = new aws.S3({endpoint: ep, region: 'us-east-1'});
-    var myBucket = 'web-images';
+    var myBucket = 'web-images-olaph';
 
     var upload = multer({
         storage: multerS3({
@@ -22,7 +36,7 @@ var galleryController = function(title) {
 
         var imageUrlList = [];
         var params = {Bucket: myBucket};
-        s3.listObjectsV2(params, function (err, data) {
+        s3.listObjects(params, function (err, data) {
             if(data) {
                 var bucketContents = data.Contents;
 
